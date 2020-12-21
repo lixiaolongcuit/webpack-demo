85de12c9cf1a6d3e7b1d188414ba92e18ba87ed8
lixiaolong <lixiaolong@skyguard.com.cn>
2020-12-21 13:53:20 +0800

    加载css文件

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
@@ -6,6 +7,7 @@ function component() {
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
     // lodash，现在通过一个 script 引入
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
   
     return element;
 }
